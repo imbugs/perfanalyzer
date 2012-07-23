bcc7f2b77b5ae19ab96bc359cc895ecadbec810c
tinghe <tinghe@ALIPAY-24033.(none)>
2012-07-23 14:28:00 +0800

    bugfix: 修改错误的变量引用

--- howlong.sh
+++ howlong.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 baseDir=$(cd "$(dirname "$0")"; pwd)
-file=`cat $baseDir/testcase`
+files=`cat $baseDir/testcase`
 for i in $files;do echo $i;tail $i/nmon/jstat* -n 1;done|awk '{print $1}'
